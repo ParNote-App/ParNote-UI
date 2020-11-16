ec53cdc016a518b2acf689e0ef0cce4cc81d1b14
kahverengi <ahmet.duruer@yandex.com>
2020-11-16 21:20:27 +0100

    Separate header to 2 columns and add .bg-light-primary class

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -2,8 +2,19 @@
   import Router from "routve";
   import RouterConfig from "../router.config";
 
-  import Header from "../components/Header.svelte";
+  import LeftHeader from "./LeftHeader.svelte";
+  import RightHeader from "./RightHeader.svelte";
 </script>
 
-<Header/>
+<div class="container-fluid h-100">
+  <div class="row h-100">
+    <div class="col">
+      <LeftHeader/>
+    </div>
+    <div class="col bg-light-primary">
+      <RightHeader/>
+    </div>
+  </div>
+</div>
+
 <Router routerConfig="{RouterConfig}"/>
---
+++ src/components/RightHeader.svelte
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand text-primary mt-3">
+  <div class="container-fluid">
+    <ul class="ml-auto navbar-nav">
+      <li class="nav-item dropdown">
+        <a
+          class="nav-link text-primary"
+          href="#"
+          id="navbarDropdown"
+          role="button"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          TR
+          <i class="fas fa-angle-down"></i>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">EN (US)</a>
+          <a class="dropdown-item" href="#">HU</a>
+          <a class="dropdown-item" href="#">DE</a>
+        </div>
+      </li>
+      <li class="nav-item mx-4">
+        <button class="btn btn-primary d-flex align-items-center"><i
+            class="far fa-user fa-xs mr-2"
+          ></i>
+          Login
+          <i class="fas fa-chevron-right fa-xs ml-2"></i></button>
+      </li>
+    </ul>
+  </div>
+</nav>
