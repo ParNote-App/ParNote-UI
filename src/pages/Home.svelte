e091e23864741905115b3769173388e35bd96ccf
kahverengi <ahmet.duruer@yandex.com>
2020-11-16 22:00:48 +0100

    Update home page

---
+++ src/pages/Home.svelte
@@ -0,0 +1,20 @@
+<main role="main" class="px-5">
+  <h1>Take notes easily.</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-10">
+        <p class="lead">
+          A note application which allows you to add, edit and share personal notes, collaborate with your friends and
+          moreover discover other people notes.
+        </p>
+      </div>
+    </div>
+  </div>
+  <p class="lead">
+    <a href="#" class="btn btn-lg btn-primary">Create Account</a>
+    <a href="#" class="btn btn-lg btn-outline-primary align-items-center ml-3"><i
+      class="far fa-compass mr-1"
+    ></i>
+      Discover Notes</a>
+  </p>
+</main>
