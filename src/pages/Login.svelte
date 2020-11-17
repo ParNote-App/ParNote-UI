9c6b4f4aaeb0cef0d7febd57af68a9b33b7fc7ba
TheNavyInfantry <nedimcanulusoy@gmail.com>
2020-11-18 01:32:19 +0300

    Add Login Page Design

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -1,1 +1,63 @@
-<h1>Login page</h1>
+<main role="main" class="px-9 mx-9">
+  <div class="container-fluid px-9 mx-9">
+    <div class="row">
+      <div class="col-auto">
+        <h1>Login page</h1>
+        <form>
+          <div class="form-group mb-4">
+            <label for="usernameOrEmail" class="u-font-size-90">Username or
+              Email</label>
+            <input
+              type="email"
+              class="form-control"
+              id="usernameOrEmail"
+              aria-describedby="emailHelp"
+              placeholder="Username or Email"
+            />
+          </div>
+
+          <div class="form-group mb-4">
+            <label for="password" class="u-font-size-90">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="password"
+              aria-describedby="password"
+              placeholder="Password"
+            />
+          </div>
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-12 col-sm-6 col-md-6">
+                <form>
+                  <a href="/login" class="btn btn-primary btn-lg">Login<i
+                      class="fas fa-chevron-right pl-2"
+                    ></i>
+                  </a>
+                </form>
+              </div>
+              <div class="col-12 col-sm-6 col-md-6">
+                <form>
+                  <a href="/register" class="btn btn-link btn-lg">Create Account
+                  </a>
+                </form>
+              </div>
+            </div>
+            <div class="container my-3">
+              <div class="col-md-12 text-center">
+                <form>
+                  <a
+                    href="/reset-password"
+                    class="btn btn-outline-primary"
+                  >Reset Password
+                  </a>
+                </form>
+                <!--                <button type="button" class="btn btn-outline-primary">Reset Password</button>-->
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</main>
