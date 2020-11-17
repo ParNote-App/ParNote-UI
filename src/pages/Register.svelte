336db6b1ad4ccbded46529d5519ff61d451c1cc4
TheNavyInfantry <nedimcanulusoy@gmail.com>
2020-11-17 23:08:21 +0300

    Add Register Page design

--- src/pages/Register.svelte
+++ src/pages/Register.svelte
@@ -1,1 +1,104 @@
-<h1>Create An Account</h1>
+<main role="main" class="px-9 mx-9">
+  <div class="container-fluid px-9 mx-9">
+    <div class="row">
+      <div class="col-auto">
+        <h1>Create An Account</h1>
+        <div class="alert bg-danger text-white fade show" role="alert">
+          <div class="d-flex">
+            <div class="alert__icon mr-3">
+              <i class="fas fa-exclamation-circle"></i>
+            </div>
+
+            <div class="align-self-center mr-3">
+              This is a primary alert — check it out!
+            </div>
+
+            <div class="ml-auto">
+              <button
+                type="button"
+                class="alert__close alert__close--light"
+                data-dismiss="alert"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">×</span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <form>
+          <div class="form-group mb-4">
+            <label for="name" class="u-font-size-90">Name</label>
+            <input
+              type="text"
+              class="form-control"
+              id="name"
+              aria-describedby="name"
+              placeholder="Name"
+            />
+          </div>
+
+          <div class="form-group mb-4">
+            <label for="surname" class="u-font-size-90">Surname</label>
+            <input
+              type="text"
+              class="form-control"
+              id="surname"
+              aria-describedby="surname"
+              placeholder="Surname"
+            />
+          </div>
+
+          <div class="form-group mb-4">
+            <label for="email" class="u-font-size-90">Email</label>
+            <input
+              type="email"
+              class="form-control"
+              id="email"
+              aria-describedby="emailHelp"
+              placeholder="Email"
+            />
+          </div>
+
+          <div class="form-group mb-4">
+            <label for="password" class="u-font-size-90">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="password"
+              aria-describedby="password"
+              placeholder="password"
+            />
+          </div>
+
+          <div class="custom-control custom-checkbox mb-2">
+            <input
+              type="checkbox"
+              class="custom-control-input"
+              id="customCheck2"
+              checked=""
+            />
+            <label
+              class="custom-control-label u-font-size-90"
+              for="customCheck2"
+            >I accept
+              <a href="/terms-and-policy"><b>terms.</b></a></label>
+          </div>
+          <div class="container-fluid w-100">
+            <div class="row w-100">
+              <div class="col-auto d-flex w-100 flex-column">
+                <div class="mr-auto mt-4">
+                  <button type="submit" class="btn btn-lg btn-primary">Create
+                    Account</button>
+                </div>
+                <div class="ml-auto">
+                  <a href="/login"><i class="fas fa-arrow-left"></i><b
+                    >Login</b></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</main>
