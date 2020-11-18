7600a188f9518f1a5fad4e3dfc38cb62677e4811
alphazarc <alphazarc@gmail.com>
2020-11-18 16:05:47 +0300

    Add Reset Password Page Design

--- src/pages/ResetPassword.svelte
+++ src/pages/ResetPassword.svelte
@@ -1,1 +1,16 @@
-<h1>Reset Your Password</h1>
+<main role="main" class="px-9 mx-9">
+  <div class="container-fluid px-9 mx-9">
+    <h1>Reset Password</h1>
+        <div class="form-group mb-4">
+            <label for="newPassword" class="u-font-size-90">New Password</label>
+            <input type="password" class="form-control" id="newPassword" aria-describedby="passwordHelp" placeholder="Enter new password">
+        </div>
+        <div class="form-group mb-4">
+            <label for="newPasswordRepeat" class="u-font-size-90">New Password Repeat</label>
+            <input type="password" class="form-control" id="newPasswordRepeat" aria-describedby="passwordHelp" placeholder="Repeat new password">
+        </div>
+            <div class="row">
+            </div>
+  </div>
+</main>
+
