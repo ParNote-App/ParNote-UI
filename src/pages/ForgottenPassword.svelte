266301fea41dca97d108a86d23fbe53bff77992e
archely <arda_atila@hotmail.com>
2020-11-18 12:38:16 +0100

    Edited ForgottenPassword

--- src/pages/ForgottenPassword.svelte
+++ src/pages/ForgottenPassword.svelte
@@ -1,1 +1,47 @@
-<h1>Forgotten Password?</h1>
+<main role="main" class="px-9 mx-9">
+    <div class="container-fluid px-9 mx-9">
+        <div class="row">
+            <div class="col-auto">
+                <center>
+                    <h1>Reset Password</h1>
+                </center>
+                <div class="alert bg-danger text-white fade show" role="alert">
+                    <div class="d-flex">
+                        <div class="alert__icon mr-3">
+                            <i class="fas fa-exclamation-circle"></i>
+                        </div>
+
+                        <div class="align-self-center mr-3">This is a primary alert — check it out!</div>
+
+                        <div class="ml-auto">
+                            <button type="button" class="alert__close alert__close--light" data-dismiss="alert"
+                                    aria-label="Close">
+                                <span aria-hidden="true">×</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group mb-4 ">
+                    <label for="defaultInput" class="u-font-size-90">Username / Email</label>
+                    <input type="email" class="form-control" id="defaultInput" aria-describedby="emailHelp"
+                           placeholder="Username or Email">
+                </div>
+
+                <div class="form-group mb-4 text-center">
+                    <a href="#" class="btn btn-lg btn-primary py-2 px-3" type="submit">Reset Password
+                        <i class="fas fa-chevron-right"></i></a>
+
+
+                </div>
+                <div class="form-group mb-4 text-center">
+
+                    <a href="/register" class="btn btn-group-sm btn-primary py-1 px-2" type="submit">Create Account</a>
+
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+</main>
