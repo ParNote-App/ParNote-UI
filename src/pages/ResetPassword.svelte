574eb849dd6ed22145a8f2b208bce3a84fb5019b
alphazarc <alphazarc@gmail.com>
2020-11-18 16:58:54 +0300

    Add Reset Password button to Reset Password Page

--- src/pages/ResetPassword.svelte
+++ src/pages/ResetPassword.svelte
@@ -9,6 +9,9 @@
             <label for="newPasswordRepeat" class="u-font-size-90">New Password Repeat</label>
             <input type="password" class="form-control" id="newPasswordRepeat" aria-describedby="passwordHelp" placeholder="Repeat new password">
         </div>
+        <a class="btn btn-primary mx-auto my-3 d-block" href="/" role="button">Reset Password</a>
+        <a class="btn btn-outline-primary mx-auto my-3 d-block" href="/register" role="button">Create Account</a>
+
             <div class="row">
             </div>
   </div>
