8869e4bf8b035f87a4dfb0eee0d89b7a43deac37
archely <arda_atila@hotmail.com>
2020-11-18 18:25:38 +0100

    Finish About design

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,1 +1,48 @@
-<h1>About Us</h1>
+<main role="main">
+  <div class="container-fluid">
+    <div class="row justify-content-center">
+      <div class="col-auto">
+        <div class="text-center">
+          <img
+            class="img-responsive "
+            alt="Parnote"
+            src="../assets/img/logo.svg"
+            style="width: 60px"
+          />
+        </div>
+        <h1 class="text-center pt-2">About Parnote</h1>
+        <div class="container-fluid my-3">
+          <div class="row justify-content-center">
+            <div class="col-auto text-center mt-3">
+              <p class="lead">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                dapibus, neque aliquam suscipit auctor, mauris purus sagittis
+                orci, a porta metus nulla nec odio. Vivamus pharetra sodales
+                velit, eu vestibulum odio porta vel. Phasellus dignissim justo
+                eu eros molestie, quis dapibus ante dapibus. Nulla rhoncus velit
+                urna, non placerat nisi rutrum at.
+              </p>
+              <hr class="mt-2 mb-3" />
+              <p class="lead">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                dapibus, neque aliquam suscipit auctor, mauris purus sagittis
+                orci, a porta metus nulla nec odio. Vivamus pharetra sodales
+                velit, eu vestibulum odio porta vel. Phasellus dignissim justo
+                eu eros molestie, quis dapibus ante dapibus. Nulla rhoncus velit
+                urna, non placerat nisi rutrum at. Suspendisse mollis elit in
+              </p>
+              <hr class="mt-2 mb-3" />
+              <p class="lead">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                dapibus, neque aliquam suscipit auctor, mauris purus sagittis
+                orci, a porta metus nulla nec odio. Vivamus pharetra sodales
+                velit, eu vestibulum odio porta vel. Phasellus dignissim justo
+                eu eros molestie, quis dapibus ante dapibus. Nulla rhoncus velit
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
