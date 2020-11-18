e0ea12a7193866896b34881fdb85d45898f448bf
alphazarc <alphazarc@gmail.com>
2020-11-18 17:22:23 +0300

    Add Error Message to Reset Password Page

--- src/pages/ResetPassword.svelte
+++ src/pages/ResetPassword.svelte
@@ -1,6 +1,21 @@
 <main role="main" class="px-9 mx-9">
   <div class="container-fluid px-9 mx-9">
-    <h1>Reset Password</h1>
+    <h1 class="text-center">Reset Password</h1>
+      <div class="alert bg-danger text-white fade show rounded-0" role="alert">
+          <div class="container d-flex">
+              <div class="alert__icon mr-3">
+                  <i class="fas fa-exclamation-circle"></i>
+              </div>
+
+              <div class="align-self-center mr-3">This is a danger alert — check it out!</div>
+
+              <div class="ml-auto">
+                  <button type="button" class="alert__close alert__close--light" data-dismiss="alert" aria-label="Close">
+                      <span aria-hidden="true">×</span>
+                  </button>
+              </div>
+          </div>
+      </div>
         <div class="form-group mb-4">
             <label for="newPassword" class="u-font-size-90">New Password</label>
             <input type="password" class="form-control" id="newPassword" aria-describedby="passwordHelp" placeholder="Enter new password">
