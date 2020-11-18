e2be8e0ef1fde0bcbef28cb267088bb49024a68b
alphazarc <alphazarc@gmail.com>
2020-11-18 18:51:35 +0300

    Add Activate Email Page

--- src/pages/ActivateEmail.svelte
+++ src/pages/ActivateEmail.svelte
@@ -1,1 +1,9 @@
-<h1>Activate Your E-Mail</h1>
+<main role="main" class="px-9 mx-9">
+  <div class="container-fluid px-9 mx-9">
+    <h1 class="text-center"><strong>Verify your email</strong></h1>
+    <h2 class="text-center">
+      You will need to verify your email to complete registration
+    </h2>
+    <div class="row"></div>
+  </div>
+</main>
