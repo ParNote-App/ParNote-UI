21f8f25b3d57141c9a740b87f0f65d2383be4838
kahverengi <ahmet.duruer@yandex.com>
2020-11-21 20:01:02 +0100

    Review ActivateEmail page

--- src/pages/ActivateEmail.svelte
+++ src/pages/ActivateEmail.svelte
@@ -1,9 +1,16 @@
-<main role="main" class="px-9 mx-9">
-  <div class="container-fluid px-9 mx-9">
-    <h1 class="text-center"><strong>Verify your email</strong></h1>
-    <h2 class="text-center">
-      You will need to verify your email to complete registration
-    </h2>
-    <div class="row"></div>
+<main role="main">
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h1><strong>Verify your email</strong></h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-3"></div>
+      <div class="col-6">
+        <p class="lead">You will need to verify your email to complete registration.</p>
+      </div>
+      <div class="col-3"></div>
+    </div>
   </div>
 </main>
