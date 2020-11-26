05c30cb05229a1905a7435b1e2f84d7fee695731
kahverengi <ahmet.duruer@yandex.com>
2020-11-26 20:04:26 +0100

    Remove recaptcha texts for now

--- src/components/Recaptcha.svelte
+++ src/components/Recaptcha.svelte
@@ -50,10 +50,10 @@
   });
 </script>
 
-This site is protected by reCAPTCHA and the Google
-<a href="https://policies.google.com/privacy" target="_blank">Privacy Policy</a>
-and
-<a href="https://policies.google.com/terms" target="_blank">Terms of Service</a>
-apply.
+<!--This site is protected by reCAPTCHA and the Google-->
+<!--<a href="https://policies.google.com/privacy" target="_blank">Privacy Policy</a>-->
+<!--and-->
+<!--<a href="https://policies.google.com/terms" target="_blank">Terms of Service</a>-->
+<!--apply.-->
 
 <div id="{recaptcha + id}"></div>
