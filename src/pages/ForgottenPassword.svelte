f98214f90cfb2fa4b6ed0d353d465e0bb16b587f
kahverengi <ahmet.duruer@yandex.com>
2020-12-01 23:30:02 +0100

    Add UI logics to forgotten password page

--- src/pages/ForgottenPassword.svelte
+++ src/pages/ForgottenPassword.svelte
@@ -1,52 +1,120 @@
+<script>
+  import { get, writable } from "svelte/store";
+
+  import SuccessAlert, {
+    hide as hideSuccess,
+    show as showSuccess,
+  } from "../components/SuccessAlert.svelte";
+  import ErrorAlert, {
+    hide as hideError,
+    show as showError,
+  } from "../components/ErrorAlert.svelte";
+
+  import Recaptcha, {
+    execute as executeRecaptcha,
+    reset as resetRecaptcha,
+  } from "../components/Recaptcha.svelte";
+
+  import ApiUtil, { NETWORK_ERROR } from "../util/api.util";
+
+  export let token = null;
+
+  const recaptchaID = writable(0);
+  const data = {
+    token: token,
+    usernameOrEmail: "",
+    recaptcha: "",
+  };
+
+  let buttonsLoading = false;
+
+  function submit() {
+    buttonsLoading = true;
+
+    hideError();
+    hideSuccess();
+    resetRecaptcha(get(recaptchaID));
+    executeRecaptcha(get(recaptchaID));
+  }
+
+  function recaptchaCallback(event) {
+    data.recaptcha = event.detail.token;
+
+    resetPassword();
+  }
+
+  function recaptchaErrorCallback() {
+    hideSuccess();
+    hideError();
+
+    buttonsLoading = false;
+
+    showError(NETWORK_ERROR);
+  }
+
+  function resetPassword() {
+    ApiUtil.post("auth/forgotPassword", data)
+      .then((response) => {
+        if (response.data.result === "ok") {
+          showSuccess("EMAIL_SENT_SUCCESSFULLY");
+        } else {
+          const errorCode = response.data.error;
+
+          showError(errorCode);
+        }
+
+        buttonsLoading = false;
+      })
+      .catch(() => {
+        showError(NETWORK_ERROR);
+
+        buttonsLoading = false;
+      });
+  }
+</script>
+
 <main role="main">
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-auto">
         <h1 class="text-center">Reset Password</h1>
-
-        <div class="alert bg-danger text-white fade show" role="alert">
-          <div class="d-flex">
-            <div class="alert__icon mr-3">
-              <i class="fas fa-exclamation-circle"></i>
-            </div>
-
-            <div class="align-self-center mr-3">
-              This is a primary alert — check it out!
-            </div>
-
-            <div class="ml-auto">
-              <button
-                type="button"
-                class="alert__close alert__close--light"
-                data-dismiss="alert"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">×</span>
-              </button>
-            </div>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-6">
+        <SuccessAlert />
+        <ErrorAlert />
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-5">
+        <form on:submit|preventDefault="{submit}">
+          <div class="form-group mb-4 ">
+            <label for="usernameOrEmail" class="u-font-size-90">Username / E-mail</label>
+            <input
+              type="text"
+              class="form-control"
+              id="usernameOrEmail"
+              aria-describedby="usernameOrEmail"
+              bind:value="{data.usernameOrEmail}"
+            />
           </div>
-        </div>
 
-        <div class="form-group mb-4 ">
-          <label for="defaultInput" class="u-font-size-90">Username / Email</label>
-          <input
-            type="email"
-            class="form-control"
-            id="defaultInput"
-            aria-describedby="emailHelp"
-            placeholder="Username or Email"
-          />
-        </div>
-
-        <div class="text-center">
-          <a
-            href="/reset-password"
-            class="btn btn-lg btn-primary"
+          <button
             type="submit"
+            class="btn btn-lg btn-primary d-block mx-auto"
+            class:disabled="{buttonsLoading}"
+            disabled="{buttonsLoading}"
           >Reset Password
             <i class="fas fa-chevron-right"></i>
-          </a>
-        </div>
+          </button>
+
+          <Recaptcha
+            recaptchaID="{recaptchaID}"
+            on:callback="{recaptchaCallback}"
+            on:errorCallback="{recaptchaErrorCallback}"
+          />
+        </form>
       </div>
     </div>
   </div>
