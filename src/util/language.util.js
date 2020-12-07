b9baef50cb11b75dd937729979e0d74e2afa9d10
kahverengi <ahmet.duruer@yandex.com>
2020-12-07 15:58:29 +0300

    Upgrade language util

--- src/util/language.util.js
+++ src/util/language.util.js
@@ -1,4 +1,5 @@
-import { init, getLocaleFromNavigator, addMessages } from "svelte-i18n";
+import { init, addMessages, locale, getLocaleFromNavigator } from "svelte-i18n";
+import { get } from "svelte/store";
 
 import EN from "../lang/en.json";
 import TR from "../lang/tr.json";
@@ -6,13 +7,29 @@ import DE from "../lang/de.json";
 import RU from "../lang/ru.json";
 import HU from "../lang/hu.json";
 
-addMessages("en", EN);
-addMessages("tr", TR);
-addMessages("de", DE);
-addMessages("ru", RU);
-addMessages("hu", HU);
+const languages = {
+  tr: TR,
+  en: EN,
+  de: DE,
+  ru: RU,
+  hu: HU,
+};
+
+Object.keys(languages).forEach((lang) => {
+  addMessages(lang, languages[lang]);
+});
+
+export function convertNavigation(currentLocale = get(locale)) {
+  let lang = "en";
+
+  Object.keys(languages).forEach((langKey) => {
+    if (currentLocale.toLowerCase().startsWith(langKey)) lang = langKey;
+  });
+
+  return lang;
+}
 
 init({
   fallbackLocale: "en",
-  initialLocale: getLocaleFromNavigator(),
+  initialLocale: convertNavigation(getLocaleFromNavigator()),
 });
