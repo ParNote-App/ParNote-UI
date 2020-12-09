4b38b9c899c9083e36b887ed075bd065d51e01b3
ultub <selimgokcek@outlook.com>
2020-12-09 15:15:33 +0300

    Note modal

--- src/pages/loggedIn/Home.svelte
+++ src/pages/loggedIn/Home.svelte
@@ -118,7 +118,12 @@
             <!-- Note Layout Starts Here -->
 
             <div class="card-columns">
-              <a href="#" class="text-dark">
+              <a
+                href="#"
+                class="text-dark"
+                data-toggle="modal"
+                data-target="#noteModal"
+              >
                 <div class="card note-card">
                   <div class="card-body">
                     <h5 class="card-title font-weight-bolder">Note title</h5>
@@ -175,6 +180,49 @@
       </div>
     </div>
   </main>
+
+  <div
+    class="modal"
+    id="noteModal"
+    tabindex="-1"
+    aria-labelledby="noteModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <input
+            type="text"
+            class="form-control form-control-lg bg-transparent border-0 font-weight-bolder p-1 modal-note-content"
+            placeholder="Note title"
+          />
+          <button
+            type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <input
+            type="text"
+            class="form-control bg-transparent border-0 font-weight-normal p-1 modal-note-content"
+            placeholder="..."
+          />
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-link" data-dismiss="modal">
+            <i class="fas fa-archive mr-2"></i>
+            Arşiv</button>
+          <button type="button" class="btn btn-danger">
+            <i class="far fa-trash-alt mr-2"></i>
+            Çöp</button>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <footer>
     <div class="container d-flex">
