a8187821a4e8171f1b29de95a4870112bc28801a
kahverengi <ahmet.duruer@yandex.com>
2020-12-09 12:50:24 +0300

    Add settings page after logged in

--- src/router.config.loggedIn.js
+++ src/router.config.loggedIn.js
@@ -1,9 +1,10 @@
-import Home from "./pages/loggedIn/Home.svelte"
-
 export default {
   routes: {
     "/": {
-      component: Home,
+      component: () => import("./pages/loggedIn/Home.svelte"),
+    },
+    "/settings": {
+      component: () => import("./pages/loggedIn/Settings.svelte"),
     },
     "*": {
       component: () => import("./pages/loggedIn/Error404.svelte"),
