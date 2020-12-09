202a0a28686188ffea49a876b5e76597ba82e1d2
kahverengi <ahmet.duruer@yandex.com>
2020-12-09 22:11:27 +0300

    Add getNotes on home page after logged in

--- src/pages/loggedIn/Home.svelte
+++ src/pages/loggedIn/Home.svelte
@@ -4,9 +4,35 @@
   import { isPageInitialized } from "../../Store";
 
   import NoteModal from "../../components/modals/NoteModal.svelte";
+  import ApiUtil from "../../util/api.util";
+
+  let notes = [];
 
   onMount(() => {
-    isPageInitialized.set(true);
+    (function getData() {
+      ApiUtil.post("user/getNotes", {})
+        .then((response) => {
+          if (response.data.result === "ok") {
+            notes = [];
+
+            response.data.notes.forEach((note) => {
+              notes.push(note);
+            });
+
+            isPageInitialized.set(true);
+          } else {
+            console.log(response.data);
+            setTimeout(() => {
+              getData();
+            }, 500);
+          }
+        })
+        .catch(() => {
+          setTimeout(() => {
+            getData();
+          }, 500);
+        });
+    })();
   });
 </script>
 
@@ -65,36 +91,38 @@
     <div class="container">
       <!-- Note Layout Starts Here -->
 
-      <div class="card-columns">
-        <a
-          href="#"
-          class="text-dark"
-          data-toggle="modal"
-          data-target="#noteModal"
-        >
-          <div class="card note-card">
-            <div class="card-body">
-              <h5 class="card-title font-weight-bolder">Note title</h5>
-              <p class="card-text">
-                Note content here lorem impsum dolor sit amet. Bla bla bla
-              </p>
-              <p class="card-text">
-                <small class="text-muted">Last updated 3 mins ago</small>
-              </p>
+      {#if notes.length > 1}
+        <div class="card-columns">
+          <a
+            href="#"
+            class="text-dark"
+            data-toggle="modal"
+            data-target="#noteModal"
+          >
+            <div class="card note-card">
+              <div class="card-body">
+                <h5 class="card-title font-weight-bolder">Note title</h5>
+                <p class="card-text">
+                  Note content here lorem impsum dolor sit amet. Bla bla bla
+                </p>
+                <p class="card-text">
+                  <small class="text-muted">Last updated 3 mins ago</small>
+                </p>
+              </div>
             </div>
-          </div>
-        </a>
-      </div>
-
-      <div class="text-center py-4">
-        <h4 class="font-weight-bolder">Not yok.</h4>
-        <p>Not bulanamadı.</p>
-        <br />
-        <a class="btn btn-outline-primary" href="#"><i
-            class="fas fa-plus mr-1"
-          ></i>
-          Yeni Not</a>
-      </div>
+          </a>
+        </div>
+      {:else}
+        <div class="text-center py-4">
+          <h4 class="font-weight-bolder">Not yok.</h4>
+          <p>Not bulanamadı.</p>
+          <br />
+          <a class="btn btn-outline-primary" href="#"><i
+              class="fas fa-plus mr-1"
+            ></i>
+            Yeni Not</a>
+        </div>
+      {/if}
     </div>
   </div>
 
