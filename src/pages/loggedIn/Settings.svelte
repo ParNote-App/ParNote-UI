3a671ca8b4ac7b2c4de25759099853502926c577
kahverengi <ahmet.duruer@yandex.com>
2020-12-10 01:33:38 +0300

    Add change password logic in settings page

--- src/pages/loggedIn/Settings.svelte
+++ src/pages/loggedIn/Settings.svelte
@@ -1,8 +1,24 @@
 <script>
   import { onMount } from "svelte";
-  import { isPageInitialized } from "../../Store";
 
-  import { userData } from "../../Store";
+  import { isPageInitialized, userData } from "../../Store";
+  import ApiUtil from "../../util/api.util";
+
+  import SuccessAlert, {
+    show as showSuccess,
+    hide as hideSuccess,
+  } from "../../components/SuccessAlert.svelte";
+  import ErrorAlert, {
+    show as showError,
+    hide as hideError,
+  } from "../../components/ErrorAlert.svelte";
+
+  let changePasswordData = {
+    currentPassword: "",
+    newPassword: "",
+    newPasswordRepeat: "",
+  };
+  let changePasswordButtonLoading = false;
 
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -11,6 +27,40 @@
   onMount(() => {
     isPageInitialized.set(true);
   });
+
+  function changePassword() {
+    hideError();
+    hideSuccess();
+
+    changePasswordButtonLoading = true;
+
+    (function change() {
+      ApiUtil.post("user/settings/changePassword", changePasswordData)
+        .then((response) => {
+          if (response.data.result === "ok") {
+            changePasswordButtonLoading = false;
+
+            changePasswordData.currentPassword = "";
+            changePasswordData.newPassword = "";
+            changePasswordData.newPasswordRepeat = "";
+
+            showSuccess("SUCCESSFULLY_CHANGED_PASSWORD");
+          } else if (response.data.result === "error") {
+            changePasswordButtonLoading = false;
+
+            showError(response.data.error);
+          } else
+            setTimeout(() => {
+              change();
+            }, 500);
+        })
+        .catch(() => {
+          setTimeout(() => {
+            change();
+          }, 500);
+        });
+    })();
+  }
 </script>
 
 <div class="pt-3">
@@ -26,6 +76,9 @@
       olarak giriş yapıldı.
     </div>
 
+    <SuccessAlert />
+    <ErrorAlert />
+
     <div class="row">
       <div class="col-lg-6 col-12 mb-3">
         <div class="card-body">
@@ -33,15 +86,25 @@
 
           <!-- Title Here -->
 
-          <form action="">
+          <form on:submit|preventDefault="{changePassword}">
             <div class="form-row align-items-end">
               <div class="form-group col-md-4">
                 <label for="oldPassword">Current Password</label>
-                <input type="password" class="form-control" id="oldPassword" />
+                <input
+                  type="password"
+                  class="form-control"
+                  id="oldPassword"
+                  bind:value="{changePasswordData.currentPassword}"
+                />
               </div>
               <div class="form-group col-md-4">
                 <label for="newPassword">New Password</label>
-                <input type="password" class="form-control" id="newPassword" />
+                <input
+                  type="password"
+                  class="form-control"
+                  id="newPassword"
+                  bind:value="{changePasswordData.newPassword}"
+                />
               </div>
               <div class="form-group col-md-4">
                 <label for="repeatPassword">Repeat Password</label>
@@ -49,11 +112,17 @@
                   type="password"
                   class="form-control"
                   id="repeatPassword"
+                  bind:value="{changePasswordData.newPasswordRepeat}"
                 />
               </div>
             </div>
             <div class="form-group">
-              <div class="btn btn-primary" type="submit">Save</div>
+              <button
+                type="submit"
+                class="btn btn-primary"
+                class:disabled="{changePasswordButtonLoading}"
+                disabled="{changePasswordButtonLoading}"
+              >Save</button>
             </div>
           </form>
         </div>
