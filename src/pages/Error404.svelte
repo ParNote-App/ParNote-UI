7037f6c762cf189c93bd0058e123bd72c0d233b0
archely <arda_atila@hotmail.com>
2020-12-12 17:17:57 +0300

    Review Error404 page

--- src/pages/Error404.svelte
+++ src/pages/Error404.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <div class="container py-4">
-  <h4 class="font-weight-bolder">Error: 404</h4>
+  <h1 class="font-weight-bolder">Error: 404</h1>
   <p>Page not found.</p>
   <hr />
   <a href="/" class="btn btn-primary">Go back</a>
