5f88d43dcfade98455fd0eba19ca73e75c34ea26
archely <arda_atila@hotmail.com>
2020-12-12 17:18:59 +0300

    Review Home page

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -9,8 +9,8 @@
   </p>
   <div class="mt-4">
     <a href="/register" class="btn btn-primary">{$_("pages.home.links.create-account")}</a>
-    <a href="javascript:void(0);" class="btn btn-link align-items-center">
-      <i class="far fa-compass mr-1"></i>
-      {$_("pages.home.links.discover-notes")}</a>
-  </div>
+<!--    <a href="javascript:void(0);" class="btn btn-link align-items-center">-->
+<!--      <i class="far fa-compass mr-1"></i>-->
+<!--      {$_("pages.home.links.discover-notes")}</a>-->
+<!--  </div>-->
 </main>
