9f38cddf7f07d0fd387e83d774d46f47384e2dcb
archely <arda_atila@hotmail.com>
2020-12-12 17:12:55 +0300

    Review TermsAndPolicy page

--- src/pages/TermsAndPolicy.svelte
+++ src/pages/TermsAndPolicy.svelte
@@ -1,8 +1,16 @@
 <script>
-  import {_} from "svelte-i18n"
+  import { _ } from "svelte-i18n";
 </script>
 
 <main class="px-lg-5 px-3 text-center">
-    <h1>{$_("pages.terms-and-policy.title")}</h1>
-    <p>{$_("pages.terms-and-policy.body")}</p>
+  <h1>{$_('pages.terms-and-policy.title')}</h1>
+  <p>{$_('pages.terms-and-policy.body')}</p>
+
+  <p>
+    This site is protected by reCAPTCHA and the Google
+    <a href="https://policies.google.com/privacy" target="_blank">Privacy Policy</a>
+    and
+    <a href="https://policies.google.com/terms" target="_blank">Terms of Service</a>
+    apply.
+  </p>
 </main>
